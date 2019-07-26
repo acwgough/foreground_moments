found scaling mistake in 0x2 term, now no longer far too big. Also 0x2 is expressible in terms of zeta function for powerlaw beta. Corrections 0x0+1x1+0x2 now stay under anafast results, but 0x2 ~ 1e-1--1e-2 1x1 so corrections from 0x2 do not seem helpful. Currently looking for further missing factors in code

foreground_functions.py
@@ -129,7 +129,7 @@ def realisation(N, freqs, ell_max=ell_max_default):
     instance = np.zeros((N,len(ells),len(freqs)))
     #instance[0,:,i] picks out the 0th realisation of the ith frequency
     for i in range(N):
-        maps = generate_map_full(freqs)
+        maps = map_full_white(freqs)
         for j in range(len(freqs)):
             instance[i,:,j] = hp.anafast(maps[j])
     return instance