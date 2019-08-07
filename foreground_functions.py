0bcf884319f472bb41d0f5351f0e1ea1bfe4c77f
Alexander Gough <acwgough@gmail.com>
2019-08-07 16:39:59 +0100

    Meddled with trying to get a 1d fit working by changing the function ff.model_single. Deleting the optional arguments in the autoixj functions seemed to help, in that it now runs, but I do not think this is running properly as the fit parameters are far from the input ones...

--- foreground_functions.py
+++ foreground_functions.py
@@ -283,13 +283,16 @@ def model(freqs, A, alpha, beta, gamma):
     model  = mom0x0 + mom1x1 + mom0x2
     return model
 
-def model_single(ells, A, alpha, beta, gamma, nu0=nu0_default, sigma=sigma_default, nside=nside_default):
+
+#function for producing something we can hopefully do a 1d curve fit to...
+#not currently working, though deleting the optional arguments got the code to even run.
+def model_single(ells, A, alpha):
     freqs=30e9
-    ell_max = len(ells)
-    mom0x0 = auto0x0(freqs, beta=beta, ell_max=ell_max, A=A, alpha=alpha, nu0=nu0)
-    mom1x1 = auto1x1(freqs, A=A, alpha=alpha, sigma=sigma, gamma=gamma, beta=beta, ell_max=ell_max, nu0=nu0, nside=nside)
-    mom0x2 = auto0x2(freqs, A=A, alpha=alpha, ell_max=ell_max, nu0=nu0, beta=beta, sigma=sigma, gamma=gamma, nside=nside)
-    model  = mom0x0 + mom1x1 + mom0x2
+    mom0x0 = auto0x0(freqs, A=A, alpha=alpha)
+    mom1x1 = auto1x1(freqs, A=A, alpha=alpha)
+    mom0x2 = auto0x2(freqs, A=A, alpha=alpha)
+    # model  = mom0x0 + mom1x1 + mom0x2
+    model = mom0x0 #+ mom1x1 + mom0x2
     return model
 
 #wrtie function for full model that is independent of the auto functions as written
@@ -439,44 +442,3 @@ def chi2(data, freq, param):
         cosmic_var = 2/(2*ell+1) * model[ell]**2
         chi_square += residual[ell]**2 / cosmic_var
     return chi_square
-
-
-#want to make a matrix that collects chi^2 values for different sets of parameters (input parameter arrays)
-#we can then slice and plot this to see how chi^2 varies with the parameters
-# def chi2(freqs, alphas, betas, gammas, sigmas=sigma_default):
-#     #make sure everything is an array
-#     #the new axis means that these are now shape (1,) arrays
-#     if type(alphas) == np.float64 or type(alphas) == int:
-#         alphas = np.array(alphas)[np.newaxis]
-#     if type(betas) == np.float64 or type(betas) == int:
-#         betas = np.array(betas)[np.newaxis]
-#     if type(gammas) == np.float64 or type(gammas) == int:
-#         gammas = np.array(gammas)[np.newaxis]
-#     if type(sigmas) == np.float64 or type(sigmas) == int:
-#         sigmas = np.array(sigmas)[np.newaxis]
-#     if type(freqs) == np.float64 or type(freqs) == int:
-#         freqs = np.array(freqs)[np.newaxis]
-#     # generate some data. For each element of this chi^2 matrix we need to make its own map
-#
-#     #set up the empty chi2 matrix
-#     chi2 = np.zeros((len(freqs),len(alphas),len(betas),len(gammas),len(sigmas)))
-#     #grab the individual elements and update the chi2 matrix
-#     for alpha in alphas:
-#         for beta in betas:
-#             for gamma in gammas:
-#                 for sigma in sigmas:
-#                     data_maps = map_full_power(freqs, ell_max=ell_max_default, A=A_default, alpha=alpha, sigma=sigma, gamma=gamma, beta=beta, nu0=nu0_default, nside=nside_default)
-#                     mom0x0 = auto0x0(freqs, beta=beta, ell_max=ell_max_default, A=A_default, alpha=alpha, nu0=nu0_default)
-#                     mom1x1 = auto1x1(freqs, A=A_default, alpha=alpha, sigma=sigma, gamma=gamma, beta=beta, ell_max=ell_max_default, nu0=nu0_default, nside=nside_default)
-#                     mom0x2 = auto0x2(freqs, A=A_default, alpha=alpha_default, ell_max=ell_max_default, nu0=nu0_default, beta=beta_default, sigma=sigma_default, gamma=gamma_default, nside=nside_default)
-#                     model = mom0x0 + mom1x1 + mom0x2
-#                     for i in range(len(freqs)):
-#                         chi_square=0
-#                         # print(data_maps[i].shape)
-#                         cls = hp.anafast(data_maps[i])
-#                         residual = cls[i]-model[i]
-#
-#                         for ell in range(len(residual)):
-#                             cosmic_var = 2/(2*ell + 1) * cls[ell]
-#                             chi_square += residual[ell]**2 / cosmic_var
-#     return chi_square, chi2
